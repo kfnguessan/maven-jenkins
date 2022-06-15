9b95e3b3f423bd45353af9756d1ae7bf72477ff9
nguessan.felicien <nguessan.felicien@inetum.com>
2022-06-15 15:45:43 +0000

    hello world

--- src/main/java/com/example/maven/MavenApplication.java
+++ src/main/java/com/example/maven/MavenApplication.java
@@ -1,13 +1,20 @@
 package com.example.maven;
 
+import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MavenApplication {
+public class MavenApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MavenApplication.class, args);
 	}
+
+	@Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World!");
+    }
 
 }
